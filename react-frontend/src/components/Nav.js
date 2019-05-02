59ebc4c0fe4d92a865eb65412caa9aad241e73b7
naomijoyce <naomijoyceb@gmail.com>
2019-05-01 23:02:19 -0400

    added side bar nav style- unnecessary ? maybe

--- react-frontend/src/components/Nav.js
+++ react-frontend/src/components/Nav.js
@@ -4,8 +4,23 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div className="nav-bar">
-      <Link to="/drawingpage">Doodle!</Link>
-      <Link to="/">View All Drawings</Link>
+
+      <input type="checkbox"/>
+
+      <span></span>
+      <span></span>
+      <span></span>
+
+      <ul className="menu">
+        <Link to="/drawingpage" style={{ textDecoration: 'none'}}>
+          <li>Doodle!</li>
+        </Link>
+
+        <Link to="/" style={{ textDecoration: 'none'}}>
+          <li>View All Drawings</li>
+        </Link>
+      </ul>
+      
     </div>
   );
 };
