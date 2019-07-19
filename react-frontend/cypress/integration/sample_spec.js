bebac9056ea69ee92a0138c8cf46fc06def6cb6f
naomijoyce <naomijoyceb@gmail.com>
2019-07-19 15:08:47 -0400

    writing sample tests with cypress

---
+++ react-frontend/cypress/integration/sample_spec.js
@@ -0,0 +1,33 @@
+// describe('My First Test', function () {
+//   it('Does not do much!', function () {
+//     expect(true).to.equal(true)
+//   })
+// })
+
+// describe('My First Test', function () {
+//   it('Does not do much!', function () {
+//     expect(true).to.equal(false)
+//   })
+// })
+
+// describe('My First Test', function () {
+//   it('Visits the Kitchen Sink', function () {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
+describe('My First Test', function () {
+  it('finds the content "type"', function () {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
